1a156b9f6686d5925552735954d8f126e1a71e53
SOURAV-MONDAL-DEV <souravmondalcode@gmail.com>
2023-07-24 00:48:57 +0600

    mobile respinsive

--- src/Components/ProfilePage/ProfilePage.jsx
+++ src/Components/ProfilePage/ProfilePage.jsx
@@ -1,116 +1,81 @@
 import React from 'react';
-import cover from './img/cover.jpg'
-import avatar from './img/avatar.png'
+import cover from './img/cover.jpg';
+import avatar from './img/avatar.png';
+import './profile.css'; // Import the CSS file
 
 const ProfilePage = () => {
   return (
     <div>
       {/* Cover Photo */}
-      <div
-        style={{
-          width: '100%',
-          height: '250px',
-          position: 'relative',
-        }}
-      >
-        <img 
-          style={{
-          width: '100%',
-          height: '250px',
-          objectFit:"cover",
-          borderRadius:"20px"
-          }}
-          src={cover} />
+      <div className="cover-container">
+        <img className="cover-image" src={cover} />
 
         {/* Rounded Profile Picture */}
-        < img
-            style={{
-            width: '200px',
-            height: '200px',
-            borderRadius: '50%',
-            // backgroundColor: '#ccc',
-            position: 'absolute',
-            bottom: '-50px',
-            left: '20%', 
-            transform: 'translateX(-50%)',
-            border: '3px solid #fff',
-          }}
-          src={avatar}
-        />
+        <img className="profile-picture" src={avatar} />
       </div>
 
       {/* Person Information */}
-      <div
-        style={{
-          marginTop: '20px',
-          padding: '20px',
-          backgroundColor: '#fff',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
+      <div className="person-info-container">
         {/* Add your person information components here */}
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-          <div style={{ width: '150px', textAlign: 'start', fontSize: '18px' }}>First Name:</div>
-          <input type="text" placeholder="John" style={{ width: '300px', height: '40px' }} />
+        <div className="person-info-item">
+          <div className="person-info-label">First Name:</div>
+          <input type="text" placeholder="John" className="person-info-input" />
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-          <div style={{ width: '150px', textAlign: 'start', fontSize: '18px' }}>Last Name:</div>
-          <input type="text" placeholder="Doe" style={{ width: '300px', height: '40px' }} />
+        <div className="person-info-item">
+          <div className="person-info-label">Last Name:</div>
+          <input type="text" placeholder="Doe" className="person-info-input" />
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-          <div style={{ width: '150px', textAlign: 'start', fontSize: '18px' }}>Email:</div>
-          <input type="email" placeholder="john.doe@example.com" style={{ width: '300px', height: '40px' }} />
+        <div className="person-info-item">
+          <div className="person-info-label">Email:</div>
+          <input type="email" placeholder="john.doe@example.com" className="person-info-input" />
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-          <div style={{ width: '150px', textAlign: 'start', fontSize: '18px' }}>Phone:</div>
-          <input type="tel" placeholder="Phone number" style={{ width: '300px', height: '40px' }} />
+        <div className="person-info-item">
+          <div className="person-info-label">Phone:</div>
+          <input type="tel" placeholder="Phone number" className="person-info-input" />
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-          <div style={{ width: '150px', textAlign: 'start', fontSize: '18px' }}>DOB:</div>
-          <input type="date" style={{ width: '300px', height: '40px' }} />
+        <div className="person-info-item">
+          <div className="person-info-label">DOB:</div>
+          <input type="date" className="person-info-input" />
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-          <div style={{ width: '150px', textAlign: 'start', fontSize: '18px' }}>Last Qualification:</div>
-          <input type="text" placeholder="Highest qualification" style={{ width: '300px', height: '40px' }} />
+        <div className="person-info-item">
+          <div className="person-info-label">Last Qualification:</div>
+          <input type="text" placeholder="Highest qualification" className="person-info-input" />
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-          <div style={{ width: '150px', textAlign: 'start', fontSize: '18px' }}>Occupation:</div>
-          <input type="text" placeholder="Occupation" style={{ width: '300px', height: '40px' }} />
+        <div className="person-info-item">
+          <div className="person-info-label">Occupation:</div>
+          <input type="text" placeholder="Occupation" className="person-info-input" />
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-          <div style={{ width: '150px', textAlign: 'start', fontSize: '18px' }}>Address:</div>
-          <input type="text" placeholder="Address" style={{ width: '300px', height: '40px' }} />
+        <div className="person-info-item">
+          <div className="person-info-label">Address:</div>
+          <input type="text" placeholder="Address" className="person-info-input" />
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-          <div style={{ width: '150px', textAlign: 'start', fontSize: '18px' }}>State:</div>
-          <input type="text" placeholder="State" style={{ width: '300px', height: '40px' }} />
+        <div className="person-info-item">
+          <div className="person-info-label">State:</div>
+          <input type="text" placeholder="State" className="person-info-input" />
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-          <div style={{ width: '150px', textAlign: 'start', fontSize: '18px' }}>District:</div>
-          <input type="text" placeholder="District" style={{ width: '300px', height: '40px' }} />
+        <div className="person-info-item">
+          <div className="person-info-label">District:</div>
+          <input type="text" placeholder="District" className="person-info-input" />
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-          <div style={{ width: '150px', textAlign: 'start', fontSize: '18px' }}>Pin:</div>
-          <input type="text" placeholder="PIN code" style={{ width: '300px', height: '40px' }} />
+        <div className="person-info-item">
+          <div className="person-info-label">Pin:</div>
+          <input type="text" placeholder="PIN code" className="person-info-input" />
         </div>
 
-        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-          <div style={{ width: '150px', textAlign: 'start', fontSize: '18px' }}>Country:</div>
-          <input type="text" placeholder="Country" style={{ width: '300px', height: '40px' }} />
+        <div className="person-info-item">
+          <div className="person-info-label">Country:</div>
+          <input type="text" placeholder="Country" className="person-info-input" />
         </div>
-
       </div>
     </div>
   );
